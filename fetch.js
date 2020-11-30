ed0b5a8c91f4a33c03d753bf66242126bf5ae0e1
rasmusWU <62059075+rasmusWU@users.noreply.github.com>
2020-11-30 15:23:53 +0100

    Add files via upload

--- fetch.js
+++ fetch.js
@@ -4,6 +4,7 @@ var articleListWorld = document.querySelector(".articleList__world");
 var articleListHealth = document.querySelector(".articleList__health");
 var articleListBusiness = document.querySelector(".articleList__business");
 var articleListTravel = document.querySelector(".articleList__travel");
+var archiveBtn = document.querySelector(".archiveBtn");
     
     function fetchNews() {
       fetch("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=AZMibVeu6yQZlX6GCRiPHXXdeY4mATfY")
@@ -13,6 +14,12 @@ var articleListTravel = document.querySelector(".articleList__travel");
         .then(function (data) {
           console.log(data.results);
 
+          if (!window.localStorage.getItem("savedArticles")) {
+            var array = [];
+            array = JSON.stringify(array);
+            window.localStorage.setItem("savedArticles", array);
+          }
+
           data.results.forEach(function (result) {
             var clone = template.content.cloneNode(true);
 
@@ -20,6 +27,12 @@ var articleListTravel = document.querySelector(".articleList__travel");
             var header = clone.querySelector(".articleHeader");
             var details = clone.querySelector(".articleDetails");
             var date = clone.querySelector(".articleDate");
+            var btn = clone.querySelector(".archiveBtn");
+            btn.addEventListener("click", archiveBtnClick);
+
+            btn.dataset.title = result.title;
+            btn.dataset.abstract = result.abstract;
+            btn.dataset.published_date = result.published_date;
 
             header.innerText = result.title;
             details.innerText = result.abstract;
@@ -31,23 +44,23 @@ var articleListTravel = document.querySelector(".articleList__travel");
               date.classList.add("articleDateDark");
             }
 
-            if (window.localStorage.getItem("us", "on") && result.section == "us") {
+            if (articleListUS && window.localStorage.getItem("us", "on") && result.section == "us") {
               articleListUS.appendChild(clone);
             }
 
-            if (window.localStorage.getItem("world", "on") && result.section == "world") {
+            if (articleListWorld && window.localStorage.getItem("world", "on") && result.section == "world") {
               articleListWorld.appendChild(clone);
             }
 
-            if (window.localStorage.getItem("health", "on") && result.section == "health") {
+            if (articleListHealth && window.localStorage.getItem("health", "on") && result.section == "health") {
               articleListHealth.appendChild(clone);
             }
 
-            if (window.localStorage.getItem("business", "on") && result.section == "business") {
+            if (articleListBusiness && window.localStorage.getItem("business", "on") && result.section == "business") {
               articleListBusiness.appendChild(clone);
             }
 
-            if (window.localStorage.getItem("travel", "on") && result.section == "travel") {
+            if (articleListTravel && window.localStorage.getItem("travel", "on") && result.section == "travel") {
               articleListTravel.appendChild(clone);
             }
 
@@ -83,3 +96,20 @@ var articleListTravel = document.querySelector(".articleList__travel");
     displayToggleTravel.addEventListener("click", function (hide) {
       articleListTravel.classList.toggle("categoryDisplayOff");
     });
+
+function archiveBtnClick (event) {
+      var savedArticles = window.localStorage.getItem("savedArticles");
+      savedArticles = JSON.parse(savedArticles);
+      savedArticles.push(event.target.dataset);
+      console.log(JSON.stringify(savedArticles))
+      window.localStorage.setItem("savedArticles", JSON.stringify(savedArticles));
+    };
+
+var page = document.querySelector(".topNav h2").innerText;
+
+if (page == "Archive") {
+  console.log("archive");
+  var savedArticles = window.localStorage.getItem("savedArticles")
+  savedArticles = JSON.parse(savedArticles);
+  console.log(savedArticles);
+}
